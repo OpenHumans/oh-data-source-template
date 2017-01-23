dd641bb5d549fd1f137d5815f0b789fc54a9c9da
Madeleine Ball <mpball@gmail.com>
2017-01-22 21:19:47 -0500

    Fix whitenoise static files

--- oh_data_source/settings.py
+++ oh_data_source/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+# See also https://devcenter.heroku.com/articles/django-assets
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
